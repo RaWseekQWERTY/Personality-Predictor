766ab3fcba692137e3ca205db575fbe15c497498
rasik <rasikkayasthav@gmail.com>
2024-07-16 19:38:43 +0545

    creating prompts

---
+++ src/core/prompts.py
@@ -0,0 +1,49 @@
+def get_prompt(user_answer: list) -> str:
+    gemini_prompt = f'''
+        
+    '''
+
+    return gemini_prompt
+
+def questions() -> list:
+    """Returns list of questions"""
+    questions_options = [
+    {
+            "id": 1,
+            "question": "",
+            "options": [
+                
+            ]
+        },
+        {
+            "id": 2,
+            "question": "",
+            "options": [
+                
+            ]
+        },
+        {
+            "id": 3,
+            "question": "",
+            "options": [
+                
+            ]
+        },
+        {
+            "id": 4,
+            "question": "",
+            "options": [
+               
+            ]
+        },
+        {
+            "id": 5,
+            "question": "",
+            "options": [
+                
+            ]
+        }
+    ]
+
+
+    return questions_options
