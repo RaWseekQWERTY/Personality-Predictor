df1ca816e0c943c23e1b5572fde7f176b10eb6c1
rasik <rasikkayasthav@gmail.com>
2024-07-30 20:29:32 +0545

    adding questions and options

--- src/core/prompts.py
+++ src/core/prompts.py
@@ -10,37 +10,53 @@ def questions() -> list:
     questions_options = [
     {
             "id": 1,
-            "question": "",
+            "question": "When faced with a problem, how do you usually approach it?",
             "options": [
-                
+                "Analyze the situation and create a logical plan",
+                "Consider how others feel about the situation and look for a harmonious solution",
+                "Think creatively and brainstorm multiple solutions",
+                "Trust your instincts and act quickly"                
             ]
         },
         {
             "id": 2,
-            "question": "",
+            "question": "How do you prefer to spend your free time?",
             "options": [
+                "Reading a book or engaging in a solitary hobby",
+                "Spending time with friends or family",
+                " Exploring new activities or places",
+                "Relaxing at home and watching your favorite shows",
                 
             ]
         },
         {
             "id": 3,
-            "question": "",
+            "question": "How do you handle unexpected changes in your schedule?",
             "options": [
-                
+                "Adapt quickly and find a new plan",
+                "Feel stressed but try to accommodate everyone involved",
+                "Embrace the change and see it as an adventure",
+                "Stick to your original plan as closely as possible"
             ]
         },
         {
             "id": 4,
-            "question": "",
+            "question": "When making a decision, what do you prioritize?",
             "options": [
-               
+               "Logical reasoning and factual information",
+               "The impact on relationships and people's feelings",
+               "Innovative and unique solutions",
+               "Practicality and immediate results"
             ]
         },
         {
             "id": 5,
-            "question": "",
+            "question": "How do you usually communicate with others?",
             "options": [
-                
+                "Directly and to the point",
+                "Considerately, taking others' feelings into account",
+                "Enthusiastically and with a lot of energy",
+                " Calmly and thoughtfully"
             ]
         }
     ]
