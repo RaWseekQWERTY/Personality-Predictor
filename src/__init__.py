887d64cfa2f34a416ffda865938992d5fdbb4dc6
rasik <rasikkayasthav@gmail.com>
2024-08-03 16:58:59 +0545

    fixing template error

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-if "__main__" == __name__:
-    app.run(host='0.0.0.0', prort='5000')
--- src/web/run.py
+++ src/web/run.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from src.web import pages
 
 app = Flask(__name__)
+app.register_blueprint(pages.bp)
 
 if "__main__" == __name__:
     app.run(host='0.0.0.0', prort='5000')
